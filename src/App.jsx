6b8e872e37b8f0df0ae4ff1189bffa00a2b688c3
duycuong2094 <djkhuong1994@gmail.com>
2024-04-08 15:31:18 +0700

    FE login register hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './page/login register/Login'
+import Register from './page/register/Register'
+import { Route, Routes } from 'react-router-dom'
+import Hamburger from './components/Hamburger'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Hamburger/>
+    {/* <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+    </Routes> */}
+    
     </>
   )
 }

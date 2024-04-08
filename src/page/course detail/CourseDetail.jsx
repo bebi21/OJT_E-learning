db6668046786da565162a8de7642e9d11f2804ac
Vantrung86 <buivantrungthanhlac@gmail.com>
2024-04-08 15:38:55 +0700

    header/footer

--- src/page/course detail/CourseDetail.jsx
+++ src/page/course detail/CourseDetail.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { IoChevronForwardOutline } from "react-icons/io5";
+import Img from "../../img/Image.png";
+import Call from "../../img/Call Button.png";
+import { AiOutlinePlus } from "react-icons/ai";
+import { FiMinus } from "react-icons/fi";
 
 function CourseDetail() {
-  return <div>CourseDetail</div>;
+  return (
+    <div style={{backgroundColor:'rgb(239,235,245)',padding:"10px 40px"}}>
+      <div>
+        <h2 style={{ display: "flex", alignItems: "center", color:"rgb(188,34,40)",fontWeight:"bold"}}>
+          Home <IoChevronForwardOutline /> Courses <IoChevronForwardOutline />{" "}
+          Course A <IoChevronForwardOutline />
+        </h2>
+      </div>
+      <div style={{display: "flex",justifyContent:"space-between"}}>
+        <div>
+          <img src={Img} alt="" />
+          <img src={Call} alt="" />
+        </div>
+        <div>
+          <div style={{display: "flex",justifyContent:"space-between",backgroundColor:"rgb(255,255,255)"}}>
+            <p>How to launch a Webflow website</p>
+            <AiOutlinePlus />
+          </div>
+          <div style={{backgroundColor:"rgb(255,255,255)", marginTop:"15px"}}>
+            <div>
+              <p style={{display: "flex",justifyContent:"space-between"}}>What is Webflow and why is it the <FiMinus /></p>
+              <p>best website builder?</p>
+            </div>
+            <hr/>
+            <div>
+              <p style={{display: "flex",justifyContent:"space-between"}}>What is your favorite template from BRIX <img src={Call} alt="" /></p>
+              <p>Templates?</p>
+            </div>
+            <div>
+              <p style={{display: "flex",justifyContent:"space-between"}}>What is your favorite template from BRIX <img src={Call} alt="" /></p>
+              <p>Templates?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CourseDetail;
